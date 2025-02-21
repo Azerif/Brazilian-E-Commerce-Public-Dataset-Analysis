ca71dc29db70c7b0a9e0a6bceda54aa1ff618dde
azerif <azrianr77@gmail.com>
2025-02-21 13:36:24 +0700

    notebook and streamlit

---
+++ dashboard/create/createDF.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import datetime as dt
+
+def create_daily_orders_df(main_df):
+    main_df['order_approved_at'] = pd.to_datetime(main_df['order_approved_at'])
+    main_df.set_index('order_approved_at', inplace=True)
+    df = main_df.resample('D').agg({'customer_id': 'nunique'}).reset_index()
+    df.rename(columns={'order_approved_at': 'date', 'customer_id': 'num_customers'}, inplace=True)
+
+    return df
+    
+def create_best_selling_products_df(main_df):
+    df = (
+    main_df.groupby(['product_category_name', 'product_category_name_english'])
+    .agg(total_transactions=('order_id', 'count'))
+    .sort_values(by='total_transactions', ascending=False)
+    .reset_index()
+    )
+
+    return df
+
+def create_customer_satisfaction(main_df):
+    df = (
+        main_df.groupby('review_score')
+        .agg(review_count=('order_id', 'nunique'), avg_response_time=('review_response_time', 'mean'))
+        .sort_values(by='review_count', ascending=False)
+        .reset_index()
+    )
+    return df
+
+def create_payments_df(main_df):
+    df = (
+        main_df.groupby('payment_type')
+        .agg({
+            'order_id': 'nunique',
+            'payment_value' : ['mean', 'max', 'min']}
+        )
+        .rename(columns={'order_id' : 'total_transactions'})
+        .sort_values(by=('total_transactions', 'nunique'), ascending=False)
+        .reset_index()
+    )
+    return df
+
+def create_city_users_df(main_df):
+    df = (
+        main_df.groupby('customer_state')
+        .agg(total_users=('customer_id', 'nunique'))  
+        .sort_values(by='total_users', ascending=False)
+        .reset_index()
+    )
+    return df
+
+def create_rfm_df(main_df):
+
+    main_df['order_purchase_timestamp'] = pd.to_datetime(main_df['order_purchase_timestamp'])
+    reference_date = main_df['order_purchase_timestamp'].max() + dt.timedelta(days=1)
+
+    # Calculate RFM
+    rfm_df = main_df.groupby('customer_id').agg({
+        'order_purchase_timestamp': lambda x: (reference_date - x.max()).days,  # Recency
+        'order_id': 'count',  # Frequency
+        'price': 'sum'  # Monetary
+    }).reset_index()
+
+    # Change column names
+    rfm_df.columns = ['customer_id', 'recency', 'frequency', 'monetary']
+    return rfm_df
+
---
+++ dashboard/dashboard.py
@@ -0,0 +1,154 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import create.createDF as cr
+
+sns.set(style='darkgrid')
+data_directory = 'cleaned_data/'
+
+# Load data
+main_df = pd.read_csv(data_directory + 'merged_data.csv')
+
+min_date = main_df['order_approved_at'].min()
+max_date = main_df['order_approved_at'].max()
+
+# -- sidebar --
+with st.sidebar:
+    date_selection = st.date_input(
+        label='Date Filter',
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+    try:
+        start_date, end_date = date_selection 
+    except ValueError:
+        st.error('Invalid date range')
+        st.stop()
+
+main_df = main_df[(main_df['order_approved_at'] >= str(start_date)) & 
+                (main_df['order_approved_at'] <= str(end_date))]
+
+# create dataframes
+daily_orders_df = cr.create_daily_orders_df(main_df)  
+best_selling_products_df = cr.create_best_selling_products_df(main_df)
+payments_df = cr.create_payments_df(main_df)
+customer_satisfaction_df = cr.create_customer_satisfaction(main_df)
+city_users_df = cr.create_city_users_df(main_df)
+rfm_df = cr.create_rfm_df(main_df)
+
+st.title('Dashboard Brazil E-Commerce Public Dataset by Olist :rocket:') 
+with st.expander('View dataframe'):
+    st.dataframe(main_df)
+
+
+# --- Daily Orders ---
+st.header('Daily Orders')
+customer_count = main_df['customer_id'].nunique()
+st.subheader(f'Customer count: {customer_count}')
+
+fig = plt.figure(figsize=(12, 6))
+sns.lineplot(data=daily_orders_df, x='date', y='num_customers')
+plt.title('Order Count by Date')
+plt.xlabel(None)
+plt.ylabel(None)
+plt.xticks(rotation=45)
+st.pyplot(fig)
+
+
+# --- Top 10 Best Selling Products Categoris ---
+st.subheader('Top 10 Best Selling Product Categories')
+fig = plt.figure(figsize=(12,6))
+sns.barplot(
+    data=best_selling_products_df.sort_values(by='total_transactions', ascending=False).head(10), 
+    x='total_transactions', y='product_category_name_english', hue='product_category_name_english', 
+    legend=False, palette="BuPu_r_d")
+
+plt.xlabel('Total Transactions')
+plt.ylabel(None)
+plt.title('Top 10 Best-Selling Product Categories')
+st.pyplot(fig)
+
+
+col1, col2 = st.columns([0.4, 0.6])
+with col1:
+    # --- Payment Methods ---
+    st.subheader('Payment Methods Distribution')
+    df_filtered = payments_df[payments_df['payment_type'] != 'not_defined']
+
+    payment_labels = df_filtered['payment_type']
+    payment_counts = df_filtered[('total_transactions', 'nunique')]
+    explode = (0.1, 0, 0, 0)
+
+    fig = plt.figure(figsize=(7,6))
+    plt.pie(
+        payment_counts, explode=explode, labels=payment_labels, colors=sns.color_palette('deep'),
+        autopct='%1.1f%%', wedgeprops={'edgecolor': 'white', 'linewidth': 1.2}, 
+        shadow=True, pctdistance=0.5, radius=1.05
+    )
+
+    plt.title('Distribution of Payment Methods')
+    st.pyplot(fig)
+
+with col2:
+    # --- Customer Satisfaction ---
+    st.subheader('Customer Satisfaction based On Review Score')
+    num_colors = customer_satisfaction_df['review_score'].nunique()
+    colors = sns.color_palette('YlGnBu', n_colors=num_colors)  
+
+    fig = plt.figure(figsize=(10,5))
+    ax = sns.barplot(data=customer_satisfaction_df, x='review_score', y='review_count', hue='review_score', legend=False, palette=colors)
+
+    plt.xlabel('Review Score')
+    plt.ylabel('Number of Reviews')
+    plt.title('Customer Review Score Distribution')
+
+    for p in ax.patches:
+        ax.annotate(f'{p.get_height():,.0f}', 
+                    (p.get_x() + p.get_width()/2, p.get_height()), 
+                    xytext=(0,5), textcoords='offset points', 
+                    ha='center', fontsize=10)
+
+    st.pyplot(fig)
+
+
+# --- Total Customers by State ---
+st.subheader('Total Customers by State')
+fig = plt.figure(figsize=(12,6))
+sns.barplot(
+    data=city_users_df.sort_values(by='total_users', ascending=True),
+    x='customer_state', y='total_users', hue='customer_state', 
+    legend=False, palette='viridis')
+plt.title('Total Users by State')
+plt.xlabel('State') 
+plt.ylabel('Total Users')
+st.pyplot(fig)
+
+
+# --- RFM ---
+st.subheader('RFM Analysis of Brazil E-Commerce Customers')
+col1, col2, col3 = st.columns(3)
+# Setup plot size
+fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+
+with col1:
+    sns.histplot(rfm_df['recency'], bins=30, kde=True, ax=axes[0], color='blue')
+    axes[0].set_title('Recency Distribution')
+    axes[0].set_xlabel('Days Since Last Purchase')
+    axes[0].set_ylabel('Number of Customers')
+
+with col2:
+    sns.histplot(rfm_df['frequency'], bins=30, kde=True, ax=axes[1], color='green')
+    axes[1].set_title('Frequency Distribution')
+    axes[1].set_xlabel('Number of Purchases')
+    axes[1].set_ylabel('Number of Customers')
+
+with col3:
+    sns.histplot(rfm_df['monetary'], bins=30, kde=True, ax=axes[2], color='red')
+    axes[2].set_title('Monetary Distribution')
+    axes[2].set_xlabel('Total Spend (in Currency)')
+    axes[2].set_ylabel('Number of Customers')
+
+plt.tight_layout()
+st.pyplot(fig)
